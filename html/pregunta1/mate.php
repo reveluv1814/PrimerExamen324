15cd877d3b9b9e06960bc2c8761543527a30d76d
reveluv1814 <82788182+reveluv1814@users.noreply.github.com>
2022-04-16 19:42:59 -0400

    Primera sentencia resuelta

---
+++ html/pregunta1/mate.php
@@ -0,0 +1,35 @@
+<?php
+    include 'header.php';
+?>
+    <div class= "carrera">
+        <h1>Carrera de Matemática</h1>
+        <p>Excelencia en la Formación Profesional en el área de Matemática e investigación.</p>
+        <section id="intro">
+            <div class="intro-container">
+                <div class="imgblock" style="background-image:linear-gradient(rgba(4,9,30,0.5),rgba(4,9,30,0.5)),url(imagenes/mat.jpg);">
+                    
+                </div>
+                <div class="row">
+                    <div class="block">
+                        <div class="section-title">
+                            <h2>Misión</h2>
+                            <p>La Carrera de Matemática, como parte del Sistema Universitario público boliviano, es una institución de generación, transmisión y aplicación de conocimientos matemáticos, orientada hacia la investigación, la formación de profesionales calificados y al fortalecimiento de la enseñanza de la matemática a todo nivel.</p>
+                        </div>
+                    </div>
+
+                    <div class="block">
+                        <div class="section-title">
+                            <h2>Visión</h2>
+                            <p>Ser la Unidad nacional modelo de eficiencia, desarrollo e impacto social en Matemática que brinda formación sólida en pregrado y postgrado. Apoyar y contribuir al desarrollo científico y tecnológico de Bolivia a través de la resolución de problemas que competen a sus áreas de interés.</p>
+                        </div>
+                    </div>
+
+                </div>
+        </section>
+        <div class="reflexion">
+            <h2>La Carrera de Matemática es una Unidad Académica dependiente de la Facultad de Ciencias Puras y Naturales (FCPN) de la Universidad Mayor de San Andrés (UMSA), cuya misión principal es la formación profesional en Matemática a nivel superior y desarrollar la investigación matemática dentro del Instituto de Investigación Matemática (IIMAT). </h2>
+        </div>
+    </div>
+<?php
+    include 'footer.php';
+?>
