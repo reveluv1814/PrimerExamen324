034c921022a3c205a4a95b366f013ecf2586a7f6
reveluv1814 <neilgraneros11@gmail.com>
2022-04-18 02:05:58 -0400

    Sentencia 9 avance

---
+++ html/sentencia9/index.php
@@ -0,0 +1,31 @@
+<?php
+
+    include 'conexion.php';
+   
+if ($_SERVER['REQUEST_METHOD']=='GET')
+{
+	$pdo = new Conexion();
+	if (isset($_GET['id']))
+	{
+		$sql = $pdo->prepare("select * from persona where id_persona=:id");
+		$sql->bindValue(':id_persona',$_GET['id']);
+		$sql->execute();
+		$sql->setFetchMode(PDO::FETCH_ASSOC);
+		header("HTTP/1.1 200 existen datos");
+		echo json_encode($sql->fetchAll());
+	}
+	else
+	{
+		$sql = $pdo->prepare("select * from acceso");
+		$sql->execute();
+		$sql->setFetchMode(PDO::FETCH_ASSOC);
+		header("HTTP/1.1 200 existen datos");
+		echo json_encode($sql->fetchAll());
+	}
+}
+    /*$pdo = new conexion();
+    $sql =  $pdo->prepare("select * from acceso");
+    $sql->execute();
+    echo json_encode($sql->fetchAll());*/
+
+?>
